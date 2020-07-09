f7d7ae5a206c95ab662eea1a6e96c886f8de6ee3
Rodrigo <rodrigocb2012@gmail.com>
2020-07-09 19:18:12 -0300

    espaco

--- app/src/main/java/com/curso/app/appsantamededeus/ui/agenda/AgendaFragment.java
+++ app/src/main/java/com/curso/app/appsantamededeus/ui/agenda/AgendaFragment.java
@@ -22,6 +22,7 @@ public class AgendaFragment extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
